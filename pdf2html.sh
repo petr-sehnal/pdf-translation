1ba61227a429f74ed50a926eba5b5451f20a8c54
Petr Sehnal <petr@sehnal.eu>
2024-08-05 15:20:11 +0200

    WIP

--- pdf2html.sh
+++ pdf2html.sh
@@ -1,1 +1,1 @@
-pdf2htmlEX --embed cfijo data/columns.pdf out/columns.pdf2html.html
+pdf2htmlEX --heps 10 --veps 10 --space-threshold 0.5 --optimize-text 1 data/pictures2.pdf out/pictures2.pdf2html.html
